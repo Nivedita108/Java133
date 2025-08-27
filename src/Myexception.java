e1393aa6036d673b75510dc15690cacd650196e0
nived <niveditanig06@gmail.com>
2025-08-27 14:03:24 +0530

    27/08/2025

---
+++ src/Myexception.java
@@ -0,0 +1,24 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+public class Myexception {
+        public static void main(String[] args)throws IOException {
+            int x,y,z=0;
+            //Scanner sc=new Scanner(System.in);
+            DataInputStream dis=new DataInputStream(System.in);
+//            try{
+                x=dis.readInt();
+                y=dis.readInt();
+                z=x/y; //Exception
+                System.out.println("Exception not occur");
+            //}
+
+//            catch (Exception e){
+//                System.out.println(e.getMessage());
+//            }
+//            finally{
+//                System.out.println("Result is "+z);
+//            }
+        }
+    }
