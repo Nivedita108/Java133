79e7eaebe2d1202d83ffe279bb6497b5c3a2b85d
nived <niveditanig06@gmail.com>
2025-09-02 12:28:53 +0530

    02/09/2025 Array

---
+++ src/Unit3/Arraylist.java
@@ -0,0 +1,16 @@
+package Unit3;
+import java.util.ArrayList;
+public class Arraylist {
+    public static void main(String[] args) {
+        ArrayList<Integer> al=new ArrayList<>();
+        al.add(1);
+        al.add(12);
+        al.add(43);
+        al.add(3);
+        System.out.println(al);
+        for(int al1:al){
+            System.out.println((al1));
+        }
+
+    }
+}
