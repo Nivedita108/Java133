1fea119378cbb574a738b0fa981fe9e9b36848d1
nived <niveditanig06@gmail.com>
2025-09-23 12:21:35 +0530

    03/09/2025 Thread

--- src/Unit2/Thread1.java
+++ src/Unit2/Thread1.java
@@ -4,7 +4,14 @@ public class Thread1 extends Thread{
     public void run()
     {
         for(int i=1; i<10;i++){
-            System.out.println(i);
+            try{
+                Thread.sleep(10000);
+            }
+            catch(Exception e){
+                System.out.println(e.getMessage());
+            }
+            if(i%2==0)
+                System.out.println(i);
         }
     }
 }
